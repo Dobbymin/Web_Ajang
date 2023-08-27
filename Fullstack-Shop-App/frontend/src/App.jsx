059c4f1822e5dc7a7bb373d4f6f24269ad0985a6
Dobbymin <gmin0701@knu.ac.kr>
2023-08-27 21:18:14 +0900

    잔디 심기

--- Fullstack-Shop-App/frontend/src/App.jsx
+++ Fullstack-Shop-App/frontend/src/App.jsx
@@ -53,14 +53,6 @@ function App() {
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                 </Route>
-
-                {/* 로그인한 사람만 갈 수 있는 경로 */}
-                {/* <Route element={<ProtectedRoutes />}>
-                    <Route path="/product/upload" element={<UploadProductPage />} />
-                    <Route path="/product/:productId" element={<DetailProductPage />} />
-                    <Route path="/user/cart" element={<CartPage />} />
-                    <Route path="/history" element={<HistoryPage />} />
-                </Route> */}
             </Route>
         </Routes>
     );
