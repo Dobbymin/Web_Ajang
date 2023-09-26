645cdef275a9b8b400343ab274a63f5495664c46
Dobbymin <gmin0701@knu.ac.kr>
2023-09-27 00:43:42 +0900

    [FEAT] CSS 구조 및 설정

--- Fullstack-Shop-App/frontend/src/App.jsx
+++ Fullstack-Shop-App/frontend/src/App.jsx
@@ -1,13 +1,48 @@
 // import { useState } from 'react';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/LandingPage';
+import NotAuthRoutes from './components/NotAuthRoutes';
+import NavBar from './layout/NavBar';
+import Footer from './layout/Footer';
 
+function Layout() {
+    return (
+        <div className="flex flex-col h-screen justify-between">
+            <NavBar />
+            <main className="mb-auto w-10/12 max-w-4xl mx-auto">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
+}
 function App() {
     // const [count, setCount] = useState(0);
 
     return (
-        <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello World!</h1>
-        </div>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                {/* 로그인과 상관없이 갈 수 있는 경로 */}
+                <Route index element={<LandingPage />} />
+
+                {/* 로그인한 사람은 갈 수 없는 경로 */}
+                <Route element={<NotAuthRoutes />}>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                </Route>
+
+                {/* 로그인한 사람만 갈 수 있는 경로 */}
+                {/* <Route element={<ProtectedRoutes />}>
+                    <Route path="/product/upload" element={<UploadProductPage />} />
+                    <Route path="/product/:productId" element={<DetailProductPage />} />
+                    <Route path="/user/cart" element={<CartPage />} />
+                    <Route path="/history" element={<HistoryPage />} />
+                </Route> */}
+            </Route>
+        </Routes>
     );
 }
 
---
+++ Fullstack-Shop-App/frontend/src/components/NotAuthRoutes.jsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+const NotAuthRoutes = () => {
+    return <div>NotAuthRoutes</div>;
+};
+
+export default NotAuthRoutes;
